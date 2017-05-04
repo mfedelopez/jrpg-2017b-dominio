3942e4a5b04ec19f429fef9a752f31e103247d0e
Fernando Queirolo <queirolo.fer@gmail.com>
2017-05-04 17:27:29 -0300

    Se agrega Javadoc Peleable

--- src/main/java/dominio/Peleable.java
+++ src/main/java/dominio/Peleable.java
@@ -7,22 +7,66 @@ package dominio;
  * será como responderán
  */
 public interface Peleable {
+	/**
+	 * Metodo implementado en Personaje 
+	 * y en NonPlayableCharacter.
+	 * 
+	 * @param daño
+	 * @return Retorna un entero con el daño causado al llamador.
+	 */
 	int serAtacado(int daño);
-
+	
+	/**
+	 * Metodo implementado en Personaje 
+	 * y NonPlayableCharacter.
+	 * 
+	 * @return Retornara un entero con la salud del llamador.
+	 */
 	int getSalud();
-
+	/**
+	 * Metodo implementado en Personaje 
+	 * y NonPlayableCharacter.
+	 * 
+	 * @return Retornara un entero con la energia del llamador.
+	 */
 	int getMagia();
-
+	/**
+	 * Metodo void implementado en Personaje
+	 * y NonPlayableCharacter.
+	 */
 	void despuesDeTurno();
-
+	/**
+	 * Metodo implementado en Personaje
+	 * y NonPlayableCharacter.
+	 * @param atacado Personaje al cual el llamador atacara.
+	 * @return Retorna el daño causado del atacante al atacado.
+	 */
 	int atacar(Peleable atacado);
-
+	/**
+	 * Metodo implementado en Personaje
+	 * y NonPlayableCharacter.
+	 * @return Retorna el nivel del llamador multiplicado por la
+	 * constante MULTIPLICADOREXP.
+	 */
 	int otorgarExp();
-
+	/**
+	 * Metodo implementado en Personaje
+	 * y NonPlayableCharacter.
+	 * @return Retorna un entero con el ataque del llamador.
+	 */
 	int getAtaque();
-
+	/**
+	 * Metodo implementado en Personaje
+	 * y NonPlayableCharacter.
+	 * @param ataque Entero que será el nuevo ataque del llamador.
+	 */
 	void setAtaque(int ataque);
-
+	/**
+	 * Metodo implementado en Personaje
+	 * y NonPlayableCharacter.
+	 * 
+	 * @return Retorna un boolean si esta vivo o no el llamador. 
+	 */
 	boolean estaVivo();
 
 	String getNombre();
